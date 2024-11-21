9e70f95cf36bbff4a92b0ab34b76ef4a69d258f1
einav <einav.manor@post.runi.ac.il>
2024-11-21 21:33:12 +0200

    upload 3 HW2

--- Cheers.java
+++ Cheers.java
@@ -11,19 +11,19 @@ public class Cheers {
                          String anLetters = "AEFHILMNORSX";
                          int count  = 0;
                          Boolean didPriting = null;
-                         for (int j = 0;j <= 5; j++) { 
+                         for (int j = 0;j <= 11; j++) { 
                                  char anLetter = anLetters.charAt(j);   
                                  String anLetter1 = String.valueOf(anLetter);  
                                  if ((letter1).equals(anLetter1) ){
                                          i++;
-                                         didPriting = true;
                                          System.out.println("Give me an "+letter1+": "+letter1+"!");
+                                         didPriting = true;
                                  }else { 
                                          count = count +1;
                                          }
-                                 if (count > 5 && (didPriting == null)){
+                                 if (count > 11 && (didPriting == null)){
                                          i++;
-                                         System.out.println("Give me a "+letter1+": "+letter1+"!");
+                                         System.out.println("Give me a  "+letter1+": "+letter1+"!");
                                  }
                          }
                  }
